7d305d74b5cd48641264d2a2de166ced7075c8c0
111513 <mariahdungey01@gmail.com>
2025-02-25 14:08:15 -0500

    week 7

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Person person1 = new Person("Mariah", "mariahdungey01@gmail.com");
+        person1.display();
+
+        Business business1 = new Business("Mariah", "mariahdungey01@gmail.com", 6143276726L);
+        business1.display();
+    }
+}
